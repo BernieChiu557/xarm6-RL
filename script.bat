bcf9c366562cd75ee6c29beb39cd04c321a9e6c8
Yi-Hung-Chiu <bernie55764@gmail.com>
2024-03-27 21:45:41 -0400

    script file

---
+++ script.bat
@@ -0,0 +1,8 @@
+@REM training
+@REM python -m rl_zoo3.train --algo sac --env FetchReach-v2 --eval-freq 2000 --seed 42 --conf-file her.yaml --wandb-project-name FetcReach --track  
+@REM python -m rl_zoo3.train --algo sac --gym-packages gymnasium_xarm6 --env xArm6Reach-v1 --eval-freq 2000 --seed 42 --conf-file her.yaml --wandb-project-name XArmReach --track  
+
+
+@REM make video
+@REM python record_video.py -f logs --env FetchReach-v2 --algo sac --exp-id 3
+@REM python record_video.py -f logs --env xArm6Reach-v1 --gym-packages gymnasium_xarm6 --algo sac --exp-id 4
